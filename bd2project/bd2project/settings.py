f2261ac04fcc46a6ca27a7c59baef0875c51a66b
FaroJoaoFaro <pv20234@alunos.estgv.ipv.pt>
2023-01-06 20:01:49 +0000

    Update settings.py

--- bd2project/bd2project/settings.py
+++ bd2project/bd2project/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware', #o que faz conflito com o postman
+    'django.middleware.csrf.CsrfViewMiddleware', #o que faz conflito com o postman
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
