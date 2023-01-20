7498c235f7d3e34fa24cc1dee2acf498142da767
FaroJoaoFaro <pv20234@alunos.estgv.ipv.pt>
2023-01-20 17:43:12 +0000

    added buttons to increase and decrease quantity on shopping cart

--- bd2project/bd2app/urls.py
+++ bd2project/bd2app/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path('ativar_produto_fornecedor/<int:id_produto>', views.ativar_produto_fornecedor, name='ativar_produto_fornecedor'),
     path('editarUsers/', views.editarUsers, name='editarUsers'),
     path('editarUser/<int:id_user>', views.editarUser, name='editarUser'),
-    #path('edit_product/<int:produto_id>/<int:carrinho_id>', views.edit_product, name='edit_product'),   pus de lado por enquanto pq estava a dar um erro da treta
+    #path('edit_quantity_cart/<int:produto_id>/<int:carrinho_id>', views.edit_quantity_cart, name='edit_quantity_cart'),   #tinha posto de lado por um erro da treta
     #path('produto/adicionar_carrinho/<int:produto_id>', views.adicionar_carrinho, name='adicionar_carrinho'),
+    path('increment_quantity/<int:id_carrinho>/<int:id_produto>/', views.increment_quantity, name='increment_quantity'),
+    path('decrement_quantity/<int:id_carrinho>/<int:id_produto>/', views.decrement_quantity, name='decrement_quantity'),
 ]
