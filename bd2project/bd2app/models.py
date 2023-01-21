dec656c2e8cab244dfc9105ed3321b2a0cabbc4d
FaroJoaoFaro <pv20234@alunos.estgv.ipv.pt>
2023-01-21 23:15:58 +0000

    Update models.py

--- bd2project/bd2app/models.py
+++ bd2project/bd2app/models.py
@@ -49,12 +49,12 @@ class itens_carrinho_model(models.Model):
         db_table = 'itens_carrinho'
 
 class PedidoFornecedor(models.Model):
-    id_pedidofornecedor = models.IntegerField(primary_key=True)
+    id_pedidofornecedor = models.AutoField(primary_key=True)
     id_fornecedor = models.IntegerField()
     id_produto = models.IntegerField()
     quantidade = models.IntegerField()
-    datapedido = models.DateField()
-    dataentrega = models.DateField()
+    datapedido = models.DateTimeField()
+    dataentrega = models.DateTimeField()
     estado = models.CharField(max_length=255)
     class Meta:
         managed = False
